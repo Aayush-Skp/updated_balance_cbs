008ad95a2d3ba5d73d081423f7573a4b2a14c72f
Deadbody142 <keyrunshrestha619@gmail.com>
2025-05-13 11:58:37 +0545

    push_data

--- lib/views/pages/data_push/PushDataScreen.dart
+++ lib/views/pages/data_push/PushDataScreen.dart
@@ -120,21 +120,50 @@ class _PushDataScreenState extends State<PushDataScreen> {
   void _showErrorDialog(BuildContext context, String message) {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        title: Row(
+      builder: (context) =>
+          // AlertDialog(
+          //   title: Row(
+          //     children: [
+          //       Icon(Icons.error, color: Colors.red),
+          //       SizedBox(width: 8),
+          //       Text("Error"),
+          //     ],
+          //   ),
+          //   content: Text(message),
+          //   actions: [
+          //     TextButton(
+          //       onPressed: () => Navigator.pop(context),
+          //       child: Text("OK", style: TextStyle(color: Colors.red)),
+          //     ),
+          //   ],
+          // ),
+          AlertDialog(
+        contentPadding:
+            EdgeInsets.only(top: 20, bottom: 16, left: 24, right: 24),
+        insetPadding: EdgeInsets.symmetric(horizontal: 40),
+        title: Text(
+          "Error!",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Icon(Icons.error, color: Colors.red),
-            SizedBox(width: 8),
-            Text("Error"),
+            Text(
+              message,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Icon(
+              Icons.error, color: Colors.red,
+              // Icons.check_circle,
+              // color: CustomTheme.appThemeColorSecondary,
+              size: 48, // Larger icon
+            ),
+            SizedBox(height: 16),
           ],
         ),
-        content: Text(message),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("OK", style: TextStyle(color: Colors.red)),
-          ),
-        ],
       ),
     );
   }
